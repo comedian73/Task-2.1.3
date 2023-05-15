37139deec539c68bdbe9fcbfbb12750f7cee0f61
comedian73 <118024172+comedian73@users.noreply.github.com>
2023-05-15 22:38:19 +0300

    1

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,9 +1,14 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
 
     private Long nanoTime = System.nanoTime();
 
+    @Qualifier
     public Long getTime() {
         return nanoTime;
     }
